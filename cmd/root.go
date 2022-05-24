03d1e90d46d7e97e584e2aa7012bb180739aafac
Tony Vattathil <avattathil@gmail.com>
2022-05-24 15:09:12 -0700

    Create root and config stubs

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,36 @@
 package cmd
 
-// RootCmd represents the base command when called without any subcommands
-
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
-const version = "0.0.0-alpha1"
-
-var RootCmd = &cobra.Command{
-	Version: version,
-	Use:     "gogitty",
-	Short:   "A simple gogitty",
-	Long:    "gogitty",
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gogitty",
+	Short: "A GitHub Utility",
+	Long:  `A GitHub Utility to interact with the API written in Golang`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {},
 }
+var cfgFile string
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cobra.CheckErr(RootCmd.Execute)
+	cobra.CheckErr(rootCmd.Execute())
 }
-func init() {}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gogitty.yaml)")
+}
+
+func initConfig() {
+	fmt.Println("Initializing... Gitty up!!")
+}
--- main.go
+++ main.go
@@ -1,27 +1,10 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"github.com/tonynv/gogitty/cmd"
-)
-
-const (
-	usageTemplate = `
-Usage:{{if .Runnable}}
-  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
-  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
-Aliases:
-  {{.NameAndAliases}}{{end}}{{if .HasExample}}
-Examples:
-{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
-Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
-Flags:
-{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
-Global Flags:
-{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
-`
-)
+import "github.com/tonynv/gogitty/cmd"
 
 func main() {
 	cmd.Execute()
